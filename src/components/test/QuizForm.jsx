1479d36ce655672202bae0ee3c64d73fbbe61403
Alejandro <ftvalex@hotmail.com>
2024-06-09 23:24:42 +0200

    probando version

--- src/components/test/QuizForm.jsx
+++ src/components/test/QuizForm.jsx
@@ -141,7 +141,7 @@ const QuizForm = () => {
           </button>
         ))}
       </div>
-      <main className="w-full  p-4">
+      <main className="w-full p-4">
         {questions.length > 0 && (
           <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg p-4 md:p-8">
             <div>
